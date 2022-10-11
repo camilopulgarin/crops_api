c4c0440427ae9c00ba713f9ac46bd771676d1588
Camilo Pulgarin <kmilo-1997-18@hotmail.com>
2022-10-11 15:12:45 -0500

    init

---
+++ dist/app.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+// initializations
+const app = (0, express_1.default)();
+// settings
+app.set('port', process.env.PORT || 3000);
+// middlewares
+app.use((0, morgan_1.default)('dev'));
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(express_1.default.json());
+// routes
+app.get('/', (req, res) => {
+    res.send(`THE API is at http://localhost:${app.get('port')}`);
+});
+exports.default = app;
